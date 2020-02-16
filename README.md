eabb49a2330b94ce5f30d962434c87ad4292da8b
Hansel Junior Ospino Orellano <hansel.ospino@globant.com>
2020-02-16 10:20:15 -0500

    FEATURE - PREDICATE PART 4: Example of BiPredicate.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # java8-training
-Java 8 training Udemy course
+Java 8 training Udemy course. The course can be followed here https://www.udemy.com/share/102wE8AEYbdV1UQH4=/.
 
 ## Topics covered
 - Lambda expressions
@@ -7,3 +7,4 @@ Java 8 training Udemy course
 - Consumer
 - BiConsumer
 - Predicate
+- BiPredicate
--- src/com/java8/features/predicate/ConsumerPredicateExample.java
+++ src/com/java8/features/predicate/ConsumerPredicateExample.java
@@ -5,6 +5,7 @@ import com.java8.features.repo.PersonRepository;
 
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
@@ -13,13 +14,23 @@ public class ConsumerPredicateExample {
     public static void main(String[] args) {
         Predicate<Person> personHeight = per -> per.getHeight() >= 140;
         Predicate<Person> personGender = per -> per.getGender().equals("Male");
+        BiPredicate<Integer, String> personFilter = (height, gender) -> height >= 140 && gender.equals("Male");
         BiConsumer<String, List<String>> hobbiesConsumer = (name, hobbies) -> System.out.println(name + " " + hobbies);
         Consumer<Person> personConsumer = per -> {
             if (personHeight.and(personGender).test(per)) {
                 hobbiesConsumer.accept(per.getName(), per.getHobbies());
             }
         };
+        Consumer<Person> personConsumerBiPredicate = per -> {
+            if (personFilter.test(per.getHeight(), per.getGender())) {
+                hobbiesConsumer.accept(per.getName(), per.getHobbies());
+            }
+        };
 
-        PersonRepository.getAllPeople().forEach(personConsumer);
+        List<Person> peopleList = PersonRepository.getAllPeople();
+        System.out.println("Printing results using Consumer and Predicate...");
+        peopleList.forEach(personConsumer);
+        System.out.println("Printing results using Consumer and BiPredicate...");
+        peopleList.forEach(personConsumerBiPredicate);
     }
 }
