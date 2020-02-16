7efc0030a9e784a4b39292267b53b5b471bd02c5
Hansel Junior Ospino Orellano <hansel.ospino@globant.com>
2020-02-15 20:44:30 -0500

    FEATURE - CONSUMER PART 3: Example of consumer applied to person list.

--- src/com/java8/features/consumer/ConsumerPersonExample.java
+++ src/com/java8/features/consumer/ConsumerPersonExample.java
@@ -3,6 +3,7 @@ package com.java8.features.consumer;
 import com.java8.features.repo.Person;
 import com.java8.features.repo.PersonRepository;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 public class ConsumerPersonExample {
@@ -12,7 +13,19 @@ public class ConsumerPersonExample {
         Consumer<Person> consumer2 = (p) -> System.out.println(p.getName().toUpperCase());
         Consumer<Person> consumer3 = (p) -> System.out.println(p.getHobbies());
 
-        consumer1.andThen(consumer2.andThen(consumer3)).accept(PersonRepository.getAllPeople().get(0));
-        consumer1.andThen(consumer2).andThen(consumer3).accept(PersonRepository.getAllPeople().get(0));
+        List<Person> people = PersonRepository.getAllPeople();
+
+        consumer1.andThen(consumer2.andThen(consumer3)).accept(people.get(0));
+        consumer1.andThen(consumer2).andThen(consumer3).accept(people.get(0));
+
+        System.out.println("Printing every people information...");
+        people.forEach(consumer1);
+
+        System.out.println("Printing just male and height >= 140 people");
+        people.forEach(p -> {
+            if (p.getGender().equals("Male") && p.getHeight() >= 140) {
+                consumer2.andThen(consumer3).accept(p);
+            }
+        });
     }
 }
